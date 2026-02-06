aadd2a6fe4141f47ccb7c5d742082ca6a56d8d5d
Ansal James <ansaljame45@gmail.com>
2026-02-06 18:44:43 +0530

    completed interface -

---
+++ PFS.Application/Interface/IWishlistService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using PFS.Application.DTOs.Wishlist;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PFS.Application.Interface
+{
+    public interface IWishlistService
+    {
+        Task AddToWishlistAsync(Guid userId, Guid productId);
+        Task<List<WishlistItemResponseDto>> GetWishlistAsync(Guid userId);
+        Task RemoveFromWishlistAsync(Guid userId, Guid productId);
+    }
+}
