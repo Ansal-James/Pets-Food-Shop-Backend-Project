3a7ef194dd1985a163ae82287d4a8feddb4cd337
Ansal James <ansaljame45@gmail.com>
2026-02-07 11:06:27 +0530

    Cart Module started, Cart and CartItem entity crated

---
+++ PFS.Domain/Entites/Cart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PFS.Domain.Entites
+{
+    public class Cart
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public ICollection<CartItem> CartItems { get; set; } = new List<CartItem>();
+    }
+}
