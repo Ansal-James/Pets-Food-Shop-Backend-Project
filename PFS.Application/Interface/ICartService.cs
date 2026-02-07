438ca74b05e39798aee0088d6d0de0fd0e418e61
Ansal James <ansaljame45@gmail.com>
2026-02-07 14:17:27 +0530

    service created

---
+++ PFS.Application/Interface/ICartService.cs
@@ -0,0 +1,18 @@
+﻿using PFS.Application.DTOs.Cart;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PFS.Application.Interface
+{
+    public interface ICartService
+    {
+        Task AddToCartAsync(Guid userId, AddToCartDto dto);
+        Task UpdateCartItemAsync(Guid userId, Guid cartItemId, UpdateCartItemDto dto);
+        Task RemoveCartItemAsync(Guid userId, Guid cartItemId);
+        Task<CartResponseDto> GetCartAsync(Guid userId);
+        Task ClearCartAsync(Guid userId);
+    }
+}
