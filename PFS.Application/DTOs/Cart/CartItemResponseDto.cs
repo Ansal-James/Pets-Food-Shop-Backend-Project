56020cca248fbf68444e5945a481dc4b4b992773
Ansal James <ansaljame45@gmail.com>
2026-02-07 12:23:32 +0530

    Dto's Created to Add, Update, Responses(Cart, CartItems)

---
+++ PFS.Application/DTOs/Cart/CartItemResponseDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PFS.Application.DTOs.Cart
+{
+    public class CartItemResponseDto
+    {
+        public Guid CartItemId { get; set; }
+        public Guid ProductId { get; set; }
+        public string ProductName { get; set; } = null!;
+        public string? ProductImageUrl { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public decimal TotalPrice => Price * Quantity;
+    }
+}
