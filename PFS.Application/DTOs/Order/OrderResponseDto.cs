ab2ec256ff8042d3a9c911dc879377f4d5c24eb5
Ansal James <ansaljame45@gmail.com>
2026-02-09 15:30:35 +0530

    Dto Created -

---
+++ PFS.Application/DTOs/Order/OrderResponseDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PFS.Application.DTOs.Order
+{
+    public class OrderResponseDto
+    {
+        public Guid OrderId { get; set; }
+        public string Status { get; set; } = null!;
+        public decimal TotalAmount { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public List<OrderItemResponseDto> Items { get; set; } = new();
+    }
+}
