5b38d5966ab5f9772164f37446f2859d5cb751a8
Ansal James <ansaljame45@gmail.com>
2026-02-09 11:38:56 +0530

    entities and enums created

---
+++ PFS.Domain/Entites/Order.cs
@@ -0,0 +1,20 @@
+﻿using PFS.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PFS.Domain.Entites
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public OrderStatus Status { get; set; } = OrderStatus.Pending;
+        public decimal TotalAmount { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public ICollection<OrderItem> OrderItems { get; set; }
+            = new List<OrderItem>();
+    }
+}
---
+++ PFS.Domain/Entites/OrderItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PFS.Domain.Entites
+{
+    public class OrderItem
+    {
+        public Guid Id { get; set; }
+        public Guid OrderId { get; set; }
+        public Order Order { get; set; } = null!;
+        // Snapshot fields (important)
+        public Guid ProductId { get; set; }
+        public string ProductName { get; set; } = null!;
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public decimal TotalPrice => Price * Quantity;
+    }
+}
